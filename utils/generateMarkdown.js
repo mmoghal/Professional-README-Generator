cb472d6e20c012305e798e4084bd552a04ba21a1
mmoghal <mmoghal29@gmail.com>
2023-04-14 19:07:51 -0400

    Created Function to generate the license section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,11 +11,29 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+if (!license) {
+  return '';
+}
+
+return `- [License](#license)\n`;
+}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+  function renderLicenseSection(license) {
+    if (!license) {
+      return '';
+    }
+  
+    return `## License
+  
+  This project is licensed under the ${license} license. ${renderLicenseBadge(license)}\n`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
