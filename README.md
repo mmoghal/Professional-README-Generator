6dbb58aa24dd4282776aa138f7a887a3df2f9310
mmoghal <mmoghal29@gmail.com>
2023-04-14 19:00:44 -0400

    Added inquirer prompt Added questions

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Professional-README-Generator
-Professional README Generator
+# README Generator
+
+README generator
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+download and install index.js
+
+## Usage
+
+install node and inquirer
+
+## License
+
+This project is licensed under the MIT license.
+
+## Contributing
+
+create a pull request
+
+## Tests
+
+install on your pc
+
+## Questions
+
+For any questions or feedback, please contact me at mmoghal29@gmail.com. You can also visit my [GitHub profile](https://github.com/mmoghal).
--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Prompt the user for information about their project
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Provide installation instructions for your project:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide usage instructions for your project:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'Apache-2.0', 'GNU GPLv3', 'ISC', 'Unlicense'],
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Provide guidelines for contributing to your project:',
+    },
+    {
